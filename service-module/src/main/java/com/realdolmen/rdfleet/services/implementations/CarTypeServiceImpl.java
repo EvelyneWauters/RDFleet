c295d633aae9df3ba7614f763d82052a0d29b1da
Evelyne Wauters <evelyne.wauters@gmail.com>
2015-11-02 10:15:15 +0100

    Services - changed signature of updateEployee(employeeDTO) HTML - finally added bootstrap to login page

--- service-module/src/main/java/com/realdolmen/rdfleet/services/implementations/CarTypeServiceImpl.java
+++ service-module/src/main/java/com/realdolmen/rdfleet/services/implementations/CarTypeServiceImpl.java
@@ -6,6 +6,7 @@ import com.realdolmen.rdfleet.services.definitions.CarTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Iterator;
 import java.util.List;
